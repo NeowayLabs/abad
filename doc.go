9614749d0c2b7e7e5fa12d1adc661828ea0d5522
i4k <tiago4orion@gmail.com>
2018-05-06 16:56:40 -0300

    add basic eval
    Signed-off-by: i4k <tiago4orion@gmail.com>

--- doc.go
+++ doc.go
@@ -1,11 +1,3 @@
 // Package abad implements the javascript engine.
 package abad
 
-type (
-	// Abad interpreter
-	Abad struct{}
-)
-
-func (a *Abad) Exec(code string) (Obj, error) {
-	return nil, nil
-}
