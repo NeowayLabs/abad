e963f1ad4812cc0d6f3239c112eaff045f21ce1e
Tiago Katcipis <tiagokatcipis@gmail.com>
2018-05-21 19:13:55 -0300

    add first failing test

--- lexer/lex.go
+++ lexer/lex.go
@@ -11,3 +11,17 @@ type (
 		Value utf16.Str
 	}
 )
+
+func Lex(code utf16.Str) <-chan Tokval {
+	tokens := make(chan Tokval)
+	
+	go func() {
+		close(tokens)
+	}()
+
+	return tokens
+}
+
+func (t Tokval) Equal(other Tokval) bool {
+	return t.Type == other.Type && t.Value.Equal(other.Value)
+}
