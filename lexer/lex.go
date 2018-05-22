d00091f7df6bd1d3be04036ff25afeb2e4d1a15d
Tiago Katcipis <tiagokatcipis@gmail.com>
2018-05-22 18:26:34 -0300

    got tests passing, code sucks ass =D

--- lexer/lex.go
+++ lexer/lex.go
@@ -12,6 +12,10 @@ type (
 	}
 )
 
+func (t Tokval) Equal(other Tokval) bool {
+	return t.Type == other.Type && t.Value.Equal(other.Value)
+}
+
 func Lex(code utf16.Str) <-chan Tokval {
 	tokens := make(chan Tokval)
 	
@@ -37,9 +41,16 @@ type lexerState func() (*Tokval, lexerState)
 
 func lexerInitialState(code utf16.Str) lexerState {
 	return func() (*Tokval, lexerState) {
+		// TODO: handle empty input
+		
 		if isNumber(code[0]) {
 			return nil, decimalOrHexadecimalState(code, 1)
 		}
+		
+		if isDot(code[0]) {
+			return nil, decimalState(code, 1)
+		}
+		
 		// TODO: Almost everything =)
 		return nil, nil
 	}
@@ -54,9 +65,10 @@ func decimalOrHexadecimalState(code utf16.Str, position uint) lexerState {
 			}, nil
 		}
 		
-		if isNumber(code[position]) {
+		if isNumber(code[position]) || isDot(code[position]) {
 			return nil, decimalState(code, position + 1)
 		}
+		
 		// TODO: need more tests to validate input 0x and 0X
 		return nil, hexadecimalState(code, position) 
 	}
@@ -92,14 +104,30 @@ func decimalState(code utf16.Str, position uint) lexerState {
 }
 
 func isNumber(utf16char uint16) bool {
-	// TODO
-	return true
+	str := strFromChar(utf16char)
+	return numbers.Contains(str)
 }
 
 func isEOF(code utf16.Str, position uint) bool {
 	return position >= uint(len(code))
 }
 
-func (t Tokval) Equal(other Tokval) bool {
-	return t.Type == other.Type && t.Value.Equal(other.Value)
+func isDot(utf16char uint16) bool {
+	str := strFromChar(utf16char)
+	return dot.Equal(str)
+}
+
+func strFromChar(utf16char uint16) utf16.Str {
+	return utf16.Str([]uint16{utf16char})
+}
+
+
+var numbers utf16.Str
+var dot utf16.Str
+var exponents utf16.Str
+
+func init() {
+	numbers = utf16.NewStr("0123456789")
+	dot = utf16.NewStr(".")
+	exponents = utf16.NewStr("eE")
 }
