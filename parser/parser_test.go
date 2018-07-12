1274ad5b04b0bcacaa4a0f26a09fec0b756e5bf9
Tiago Katcipis <tiagokatcipis@gmail.com>
2018-07-11 21:15:55 -0300

    refactor parser tests to add multiple nodes tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -11,237 +11,237 @@ import (
 	"github.com/madlambda/spells/assert"
 )
 
-type testcase struct {
-	input       string
-	expected    ast.Node
-	expectedErr error
-}
 
 var E = fmt.Errorf
 
 func TestParserNumbers(t *testing.T) {
-	for _, tc := range []testcase{
+
+	runTests(t, []TestCase{
 		{
-			input:    "1",
-			expected: ast.NewIntNumber(1),
+			name: "SmallDecimal",
+			code:    "1",
+			want: ast.NewIntNumber(1),
 		},
 		{
-			input:    "1234567890",
-			expected: ast.NewIntNumber(1234567890),
+			name: "BigDecimal",
+			code:    "1234567890",
+			want: ast.NewIntNumber(1234567890),
 		},
 		{
-			input:       "1a",
-			expectedErr: E("tests.js:1:0: invalid token: 1a"),
+			name: "InvalidDecimal",
+			code:       "1a",
+			wantErr: E("tests.js:1:0: invalid token: 1a"),
 		},
 		{
-			input:    "0x0",
-			expected: ast.NewIntNumber(0),
+			name: "SmallHexadecimal",
+			code:    "0x0",
+			want: ast.NewIntNumber(0),
 		},
 		{
-			input:    "0x1234567890abcdef",
-			expected: ast.NewIntNumber(0x1234567890abcdef),
+			name: "BigHexaDecimal",
+			code:    "0x1234567890abcdef",
+			want: ast.NewIntNumber(0x1234567890abcdef),
 		},
 		{
-			input:    "0xff",
-			expected: ast.NewIntNumber(0xff),
+			name: "HexadecimalFF",
+			code:    "0xff",
+			want: ast.NewIntNumber(0xff),
 		},
 		{
-			input:    ".1",
-			expected: ast.NewNumber(0.1),
+			name: "SmallRealNumber",
+			code:    ".1",
+			want: ast.NewNumber(0.1),
 		},
 		{
-			input:    ".0000",
-			expected: ast.NewNumber(0.0),
+			name: "ZeroRealNumer",
+			code:    ".0000",
+			want: ast.NewNumber(0.0),
 		},
 		{
-			input:    "1234",
-			expected: ast.NewIntNumber(1234),
+			name: "SomeDecimal",
+			code:    "1234",
+			want: ast.NewIntNumber(1234),
 		},
 		{
-			input:    "0.12345",
-			expected: ast.NewNumber(0.12345),
+			name: "SmallRealNumberWithMultipleDigits",
+			code:    "0.12345",
+			want: ast.NewNumber(0.12345),
 		},
 		{
-			input:       "0.a",
-			expectedErr: E("tests.js:1:0: invalid token: 0.a"),
+			name: "InvalidRealNumberWithLetter",
+			code:       "0.a",
+			wantErr: E("tests.js:1:0: invalid token: 0.a"),
 		},
 		{
-			input:       "12.13.",
-			expectedErr: E("tests.js:1:0: invalid token: 12.13."),
+			name: "InvalidRealNumberWithTwoDots",
+			code:       "12.13.",
+			wantErr: E("tests.js:1:0: invalid token: 12.13."),
 		},
 		{
-			input:    "1.0e10",
-			expected: ast.NewNumber(1.0e10),
+			name: "RealNumberWithExponent",
+			code:    "1.0e10",
+			want: ast.NewNumber(1.0e10),
 		},
 		{
-			input:    "1e10",
-			expected: ast.NewNumber(1e10),
+			name: "DecimalWithExponent",
+			code:    "1e10",
+			want: ast.NewNumber(1e10),
 		},
 		{
-			input:    ".1e10",
-			expected: ast.NewNumber(.1e10),
+			name: "SmallRealNumberWithExponent",
+			code:    ".1e10",
+			want: ast.NewNumber(.1e10),
 		},
 		{
-			input:    "1e-10",
-			expected: ast.NewNumber(1e-10),
+			name: "DecimalWithNegativeExponent",
+			code:    "1e-10",
+			want: ast.NewNumber(1e-10),
 		},
 		{
-			input: "-1",
-			expected: ast.NewUnaryExpr(
+			name: "NegativeDecimalWithOneDigit",
+			code: "-1",
+			want: ast.NewUnaryExpr(
 				token.Minus, ast.NewNumber(1),
 			),
 		},
 		{
-			input: "-1234",
-			expected: ast.NewUnaryExpr(
+			name: "NegativeDecimalWithMultipleDigits",
+			code: "-1234",
+			want: ast.NewUnaryExpr(
 				token.Minus, ast.NewNumber(1234),
 			),
 		},
 		{
-			input: "-0x0",
-			expected: ast.NewUnaryExpr(
+			name: "NegativeZeroHexadecimal",
+			code: "-0x0",
+			want: ast.NewUnaryExpr(
 				token.Minus, ast.NewNumber(0),
 			),
 		},
 		{
-			input: "-0xff",
-			expected: ast.NewUnaryExpr(
+			name: "NegativeFFHexadecimal",
+			code: "-0xff",
+			want: ast.NewUnaryExpr(
 				token.Minus, ast.NewNumber(255),
 			),
 		},
 		{
-			input: "-.0",
-			expected: ast.NewUnaryExpr(
+			name: "NegativeZeroRealNumber",
+			code: "-.0",
+			want: ast.NewUnaryExpr(
 				token.Minus, ast.NewNumber(0),
 			),
 		},
 		{
-			input: "-.0e1",
-			expected: ast.NewUnaryExpr(
+			name: "NegativeZeroRealNumberWithExponent",
+			code: "-.0e1",
+			want: ast.NewUnaryExpr(
 				token.Minus, ast.NewNumber(0),
 			),
 		},
 		{
-			input:       "-12.13.",
-			expectedErr: E("tests.js:1:0: invalid token: 12.13."),
+			name: "InvalidNegativeRealNumber",
+			code:       "-12.13.",
+			wantErr: E("tests.js:1:0: invalid token: 12.13."),
 		},
 		{
-			input: "-1e-10",
-			expected: ast.NewUnaryExpr(
+			name: "NegativeDecimalWithNegativeExponent",
+			code: "-1e-10",
+			want: ast.NewUnaryExpr(
 				token.Minus, ast.NewNumber(1.0e-10),
 			),
 		},
 		{
-			input: "-+0",
-			expected: ast.NewUnaryExpr(
+			name: "NegativePlusZeroDecimal",
+			code: "-+0",
+			want: ast.NewUnaryExpr(
 				token.Minus, ast.NewUnaryExpr(
 					token.Plus, ast.NewNumber(0),
 				),
 			),
 		},
 		{
-			input: "-+-+0",
-			expected: ast.NewUnaryExpr(token.Minus,
+			name: "InterleavedNegativeWithPlusAndZeroDecimal",
+			code: "-+-+0",
+			want: ast.NewUnaryExpr(token.Minus,
 				ast.NewUnaryExpr(token.Plus,
 					ast.NewUnaryExpr(token.Minus,
 						ast.NewUnaryExpr(token.Plus,
 							ast.NewNumber(0))))),
 		},
-	} {
-		tree, err := parser.Parse("tests.js", tc.input)
-		assert.EqualErrs(t, tc.expectedErr, err, "parser err")
-
-		if err != nil {
-			continue
-		}
-
-		nodes := tree.Nodes
-		if len(nodes) != 1 {
-			t.Fatalf("number tests must be isolated: %v", nodes)
-		}
-
-		got := nodes[0]
-		if got.Type() != tc.expected.Type() {
-			t.Fatalf("literals type differ: %d != %d (%s)",
-				got.Type(), tc.expected.Type(), tc.input)
-		}
-
-		if !tc.expected.Equal(got) {
-			t.Fatalf("Numbers differ: '%s' != '%s'",
-				got, tc.expected)
-		}
-	}
+	})
 }
 
 func TestIdentifier(t *testing.T) {
-	for _, tc := range []struct {
-		input       string
-		expected    ast.Node
-		expectedErr error
-	}{
+	runTests(t, []TestCase{
 		{
-			input:    "_",
-			expected: ast.NewIdent(utf16.S("_")),
+			name: "Underscore",
+			code: "_",
+			want: ast.NewIdent(utf16.S("_")),
 		},
 		{
-			input:    "$",
-			expected: ast.NewIdent(utf16.S("$")),
+			name: "Dolar",
+			code:    "$",
+			want: ast.NewIdent(utf16.S("$")),
 		},
 		{
-			input:    "console",
-			expected: ast.NewIdent(utf16.S("console")),
+			name: "Console",
+			code:    "console",
+			want: ast.NewIdent(utf16.S("console")),
 		},
 		{
-			input:    "angular",
-			expected: ast.NewIdent(utf16.S("angular")),
+			name: "AngularSux",
+			code: "angular",
+			want: ast.NewIdent(utf16.S("angular")),
 		},
 		{
-			input:    "___hyped___",
-			expected: ast.NewIdent(utf16.S("___hyped___")),
+			name: "HyperdUnderscores",
+			code:    "___hyped___",
+			want: ast.NewIdent(utf16.S("___hyped___")),
 		},
 		{
-			input:    "a$b$c",
-			expected: ast.NewIdent(utf16.S("a$b$c")),
+			name: "LettersAndDolars",
+			code: "a$b$c",
+			want: ast.NewIdent(utf16.S("a$b$c")),
 		},
-	} {
-		testParser(t, tc.input, tc.expected, tc.expectedErr)
-	}
+	})
 }
 
 func TestMemberExpr(t *testing.T) {
-	for _, tc := range []struct {
-		input       string
-		expected    ast.Node
-		expectedErr error
-	}{
+	runTests(t, []TestCase{
 		{
-			input: "console.log",
-			expected: ast.NewMemberExpr(
+			name: "AccessingLogOnConsole",
+			code: "console.log",
+			want: ast.NewMemberExpr(
 				ast.NewIdent(utf16.S("console")),
 				ast.NewIdent(utf16.S("log")),
 			),
 		},
 		{
-			input:       "console.",
-			expectedErr: E("tests.js:1:0: unexpected EOF"),
+			name: "ErrorAccessingEmptyMember",
+			code: "console.",
+			wantErr: E("tests.js:1:0: unexpected EOF"),
 		},
 		{
-			input: "self.a",
-			expected: ast.NewMemberExpr(
+			name: "AccessMemberOfSelf",
+			code: "self.a",
+			want: ast.NewMemberExpr(
 				ast.NewIdent(utf16.S("self")),
 				ast.NewIdent(utf16.S("a")),
 			),
 		},
 		{
-			input: "self.self.self", // same as: (self.self).self
-			expected: ast.NewMemberExpr(
+			name: "OneLevelOfNesting",
+			code: "self.self.self", // same as: (self.self).self
+			want: ast.NewMemberExpr(
 				ast.NewMemberExpr(ast.NewIdent(utf16.S("self")), ast.NewIdent(utf16.S("self"))),
 				ast.NewIdent(utf16.S("self")),
 			),
 		},
 		{
-			input: "a.b.c.d.e.f", // same as: ((((a.b).c).d).e).f)
-			expected: ast.NewMemberExpr(
+			name: "MultipleLevelsOfNesting",
+			code: "a.b.c.d.e.f", // same as: ((((a.b).c).d).e).f)
+			want: ast.NewMemberExpr(
 				ast.NewMemberExpr(
 					ast.NewMemberExpr(
 						ast.NewMemberExpr(
@@ -255,20 +255,16 @@ func TestMemberExpr(t *testing.T) {
 				ast.NewIdent(utf16.S("f")),
 			),
 		},
-	} {
-		testParser(t, tc.input, tc.expected, tc.expectedErr)
-	}
+	})
 }
 
 func TestParserFuncall(t *testing.T) {
-	for _, tc := range []struct {
-		input       string
-		expected    ast.Node
-		expectedErr error
-	}{
+
+	runTests(t, []TestCase{
 		{
-			input: "console.log()",
-			expected: ast.NewCallExpr(
+			name: "MemberAccessWithoutParams",
+			code: "console.log()",
+			want: ast.NewCallExpr(
 				ast.NewMemberExpr(
 					ast.NewIdent(utf16.S("console")),
 					ast.NewIdent(utf16.S("log")),
@@ -277,8 +273,9 @@ func TestParserFuncall(t *testing.T) {
 			),
 		},
 		{
-			input: "console.log(2.0)",
-			expected: ast.NewCallExpr(
+			name: "MemberAccessWithDecimalParam",
+			code: "console.log(2.0)",
+			want: ast.NewCallExpr(
 				ast.NewMemberExpr(
 					ast.NewIdent(utf16.S("console")),
 					ast.NewIdent(utf16.S("log")),
@@ -287,8 +284,9 @@ func TestParserFuncall(t *testing.T) {
 			),
 		},
 		{
-			input: "self.console.log(2.0)",
-			expected: ast.NewCallExpr(
+			name: "NestedMemberAccessWithDecimalParam",
+			code: "self.console.log(2.0)",
+			want: ast.NewCallExpr(
 				ast.NewMemberExpr(
 					ast.NewMemberExpr(
 						ast.NewIdent(utf16.S("self")),
@@ -299,34 +297,51 @@ func TestParserFuncall(t *testing.T) {
 				[]ast.Node{ast.NewNumber(2.0)},
 			),
 		},
-	} {
-		testParser(t, tc.input, tc.expected, tc.expectedErr)
+	})
+}
+
+type TestCase struct {
+	name    string
+	code    string
+	want    ast.Node
+	wants   []ast.Node
+	wantErr error
+}
+
+func (tc *TestCase) run(t *testing.T) {
+	t.Run(tc.name, func(t *testing.T) {
+		tree, err := parser.Parse("tests.js", tc.code)
+		assert.EqualErrs(t, tc.wantErr, err, "parser err")
+
+		if err != nil {
+			return
+		}
+
+		if tc.wants == nil {
+			assertEqualNodes(t, []ast.Node{tc.want}, tree.Nodes)
+			return
+		}
+		
+		assertEqualNodes(t, tc.wants, tree.Nodes) 
+	})
+}
+
+func runTests(t *testing.T, tcases []TestCase) {
+	for _, tcase := range tcases {
+		tcase.run(t)
 	}
 }
 
-func testParser(
-	t *testing.T, input string, expected ast.Node, expectedErr error,
-) {
-	tree, err := parser.Parse("tests.js", input)
-	assert.EqualErrs(t, expectedErr, err, "parser err")
-
-	if err != nil {
-		return
+func assertEqualNodes(t *testing.T, want []ast.Node, got []ast.Node) {
+	if len(want) != len(got) {
+		t.Errorf("want[%d] nodes but got[%d] nodes", len(want), len(got))
+		t.Fatalf("want[%v] != got[%v]", want, got)
 	}
-
-	nodes := tree.Nodes
-	if len(nodes) != 1 {
-		t.Fatalf("memberexpr tests must be isolated: %v", nodes)
-	}
-
-	got := nodes[0]
-	if got.Type() != expected.Type() {
-		t.Fatalf("type differ: %d != %d (%s)",
-			got.Type(), expected.Type(), input)
-	}
-
-	if !expected.Equal(got) {
-		t.Fatalf("Identifier differ: '%s' != '%s'",
-			got, expected)
-	}
+	
+	for i, w := range want {
+		g := got[i]
+		if !w.Equal(g) {
+			t.Errorf("wanted node[%d][%v] != got node[%d][%v]", i, w, i, g)
+		}
+	} 
 }
